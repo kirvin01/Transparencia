ea40a40c452f801330c6ca115a952e32df5fc27c
Irvin C <kirvin02@gmail.com>
2024-10-15 22:53:16 -0500

    Primer commit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,14 @@
+@extends('template')
+
+@section('content')
+    <h1>Listado</h1>
+    <hr>
+    @foreach ($posts as $post)
+        <p>
+            <strong>{{ $post['id'] }}</strong>
+            <a href="{{ route('post', $post['slug']) }}">
+                {{ $post['title'] }}
+            </a>
+        </p>
+    @endforeach
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+use App\Http\Controllers\PageController;
+use Illuminate\Support\Facades\Route;
+
+Route::controller(PageController::class)->group(function () {
+    Route::get('/',             'home')->name('home');
+
+    Route::get('blog',          'blog')->name('blog');
+
+    Route::get('blog/{slug}',   'post')->name('post');;
+});
+

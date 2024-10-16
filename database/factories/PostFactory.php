1bcaf8cf3a2f09cf2c2c56bf7fe789fe6bf3a93e
Irvin <kirvin02@gmail.com>
2024-10-16 08:36:53 -0500

    MPC-commit

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title'=> $title =$this->faker->sentencel(),
+            'slug'=> Str::slug($title),
+            'body'=> $this->faker->text(2200),
+            //
+        ];
+    }
+}
---
+++ resources/views/post.blade.php
@@ -0,0 +1,7 @@
+@extends('template')
+
+@section('content')
+    <h1>Detalles</h1>
+    {{ $post }}
+  
+@endsection
